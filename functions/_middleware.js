579e7fc411de02bb95df16506ac2a17547b1723b
Luke Dary <lukedary@gmail.com>
2023-01-10 13:53:04 -0500

    Pages update

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -11,13 +11,17 @@ const arcadeFiles = async ({request,next,env}) => {
         const arcadeFile = await env.ARCADE_FILES.get(obj);
         console.log('File Found:',obj, arcadeFile.httpEtag);
         const headers = new Headers();
-        arcadeFile.writeHttpMetadata(headers)
+        arcadeFile.writeHttpMetadata(headers);
         headers.set('etag', arcadeFile.httpEtag);
+        headers.set('content-range',`bytes ${arcadeFile.size}`);
+        headers.set('content-type', obj.indexOf('wasm') >= 0 ? 'application/wasm':'');
         const status = 200;
         let response = new Response(arcadeFile.body, {
             headers,
             status
-        })
+        });
+
+        return response;
     }
     return next();
 }
